d2ccf40c0cc2b42590530d42c0a488594f165d82
Arpit Gupta <zingahugane@gmail.com>
2020-05-28 14:21:27 +0530

    updated

--- Doki_site/settings.py
+++ Doki_site/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'User.apps.UserConfig',
+    'user.apps.UserConfig',
     'First.apps.FirstConfig',
     'django.contrib.admin',
     'crispy_forms',
