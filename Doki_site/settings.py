b402d9a6a341d5cfe90c455a076e893032812413
Arpit Gupta <zingahugane@gmail.com>
2020-09-18 00:42:48 +0530

    ..

--- Doki_site/settings.py
+++ Doki_site/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Doki_site.urls'
@@ -138,6 +137,10 @@ USE_TZ = True
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
