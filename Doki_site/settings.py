2f7691f28b5cc3bcd96e6d4dbfc3de0130b2d98f
Arpit Gupta <zingahugane@gmail.com>
2020-09-18 20:32:20 +0530

    Update settings.py

--- Doki_site/settings.py
+++ Doki_site/settings.py
@@ -79,21 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Doki_site.wsgi.application'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
