db350be3d1799c642d591ab1183d8bac1e0439e4
Arpit Gupta <zingahugane@gmail.com>
2020-09-18 19:48:31 +0530

    Update settings.py

--- Doki_site/settings.py
+++ Doki_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG_VALUE')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','arpitfolio.herokuapp.com']
 
